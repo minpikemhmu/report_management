2a329a37608f6ec8d7cf1df897ddd0a238110a91
minpikemhmu <minpikehmu10@gmail.com>
2023-02-27 20:15:56 +0630

    first migration

---
+++ database/migrations/2023_02_27_130744_create_cities_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('division_state_id')
+            ->nullable()
+            ->on('division_states')
+            ->cascadeOnDelete();
+            $table->foreignId('region_id')
+            ->nullable()
+            ->on('regions')
+            ->cascadeOnDelete();
+            $table->foreignId('township_id')
+            ->nullable()
+            ->on('townships')
+            ->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+};
