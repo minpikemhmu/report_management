9c57e395fb059366c266d7a22d6b3dc0965d4e40
minpikemhmu <minpikehmu10@gmail.com>
2023-03-09 11:06:04 +0630

    Merchandiser

---
+++ app/Models/Merchandiser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Merchandiser extends Model
+{
+    use HasFactory;
+    protected $fillable = [ 'name', 'mer_code', 'region_id', 'merchant_team_id', 'merchant_area_id', 'channel_id'];
+
+    public function region(): BelongsTo
+    {
+        return $this->belongsTo(Region::class);
+    }
+
+    public function merchantTeam(): BelongsTo
+    {
+        return $this->belongsTo(MerchantTeam::class);
+    }
+
+    public function merchantArea(): BelongsTo
+    {
+        return $this->belongsTo(MerchantArea::class);
+    }
+
+    public function channel(): BelongsTo
+    {
+        return $this->belongsTo(Channel::class);
+    }
+}
