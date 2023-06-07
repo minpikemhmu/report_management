404ecf389dbf307c69e0ac2e3afb7c4b34fa9bb4
minpikemhmu <minpikehmu10@gmail.com>
2023-06-07 10:43:48 +0630

    fix login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,13 +70,13 @@
                                             {{ __('Login') }}
                                         </button>
                                     </form>
-                                    <hr>
+                                    <!-- <hr>
                                     <div class="text-center">
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
                                         <a class="small" href="register.html">Create an Account!</a>
-                                    </div>
+                                    </div> -->
                                 </div>
                             </div>
                         </div>
