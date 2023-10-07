90afee021d115bb28e82f7d32262bb931fbd109c
minpikemhmu <minpikehmu10@gmail.com>
2023-10-07 17:40:51 +0630

    spelling error

--- database/migrations/2023_10_07_172416_remove_some_foreign_key_merchandise_reports_table.php
+++ database/migrations/2023_10_07_172416_remove_some_foreign_key_merchandise_reports_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('merchandise_reports', function (Blueprint $table) {
             // Remove unnecessary columns
-            $table->dropColumn(['gondolar_type_id', 'trip_type_id', 'outstrik_type_id']);
+            $table->dropColumn(['gondolar_type_id', 'trip_type_id', 'outskirt_type_id']);
         });
     }
 
@@ -26,6 +26,11 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('merchandise_reports', function (Blueprint $table) {
+            // Add back the columns in reverse order
+            $table->integer('outstrik_type_id')->nullable();
+            $table->integer('trip_type_id')->nullable();
+            $table->integer('gondolar_type_id')->nullable();
+        });
     }
 };
